2be16e980697e1383e85e805a46b4a5dafa3706e
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2021-01-19 16:23:01 -0400

    Agregue el factory y seeder

---
+++ src/database/seeds/tasks.seeder.ts
@@ -0,0 +1,13 @@
+import Faker from 'faker';
+import { define } from 'typeorm-seeding';
+import Task from '../entities/tasks.entity';
+
+// ======================================
+//				Task Factory
+// ======================================
+define(Task, (faker: typeof Faker) => {
+	const task= new Task();
+	task.title = faker.lorem.sentence(4);
+	task.description = faker.lorem.text(10);
+	return task;
+});
